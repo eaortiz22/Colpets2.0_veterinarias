953a23ae9f6a81e09b3a20417e82f034e54cd8c0
Adrian Santamaria <adrianimpor1@gmail.com>
2025-07-24 16:30:31 -0500

    ajuste tiempo estimado

--- src/screens/veterinarians/VeterinaryDetails.tsx
+++ src/screens/veterinarians/VeterinaryDetails.tsx
@@ -120,7 +120,7 @@ const VeterinaryDetails = ({ route }: any) => {
         <MapMarkerIcon width={24} height={24} fill={theme.colors.primary} />
         <TextMedium style={{ color: theme.colors.text }}>
           {veterinary.neighborhood}, {veterinary.city}
-          {distanceInfo?.duration && `" • " ${distanceInfo?.duration}`}
+          {distanceInfo?.duration && ` • ${distanceInfo?.duration}`}
         </TextMedium>
       </View>
       <HorizontalStatsCard
